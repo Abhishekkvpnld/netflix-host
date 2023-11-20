d6fce5c2c33614dfc6ca26ab44a05533167945ff
Abhishekkv <abhishekkvpnld@gmail.com>
2023-11-20 13:27:55 +0530

    build

---
+++ build/precache-manifest.c9023350964b5fc30223246d5b161652.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3417696f3db2c4efafa9c0e2d95cdc3c",
+    "url": "/netflix-host/index.html"
+  },
+  {
+    "revision": "9ac0f79883c5d78a3eae",
+    "url": "/netflix-host/static/css/main.c71f550d.chunk.css"
+  },
+  {
+    "revision": "cb63d8a59330134747a8",
+    "url": "/netflix-host/static/js/2.e417503f.chunk.js"
+  },
+  {
+    "revision": "001c47af4bef020dd11796767485da4f",
+    "url": "/netflix-host/static/js/2.e417503f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c782cccf99c990961e36",
+    "url": "/netflix-host/static/js/3.a94a576c.chunk.js"
+  },
+  {
+    "revision": "9ac0f79883c5d78a3eae",
+    "url": "/netflix-host/static/js/main.aed7bbed.chunk.js"
+  },
+  {
+    "revision": "ea87bb07c69bd39407a2",
+    "url": "/netflix-host/static/js/runtime-main.b48de636.js"
+  }
+]);
